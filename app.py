1cee8fc4d0d7259c7c23aadff89b34610be2aef9
Евгений Кудрявцев <evgeniy-sp@yandex.ru>
2024-02-11 03:09:38 +0300

    Add user_group router and chat cleaner

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
 
+from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
 from common.bot_cmds_list import private
 
@@ -17,6 +18,7 @@ bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
+dp.include_router(user_group_router)
 
 
 async def main():
