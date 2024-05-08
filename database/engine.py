16c8475614f1f6408a3b88e47394d13b8805e3dc
Евгений Кудрявцев <evgeniy-sp@yandex.ru>
2024-05-08 18:49:58 +0300

    refactor by postgres db

--- database/engine.py
+++ database/engine.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import (
 from database.models import Base
 
 
-engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
+engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 
 session_maker = async_sessionmaker(
     bind=engine,
